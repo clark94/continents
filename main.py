af935091cb8dc71c5a622cf85fa2b6c95d0044db
Lucas Santos <gomes.lucas@outlook.com.br>
2021-06-20 18:52:55 -0300

    all

---
+++ main.py
@@ -0,0 +1,24 @@
+import world
+from world import europe
+from world.africa import zimbabwe
+
+
+def main():
+        #print(world.africa)
+        #print(world.europe)
+        print(zimbabwe.HelloZimbabwe())
+    
+ 
+
+if __name__=="__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
